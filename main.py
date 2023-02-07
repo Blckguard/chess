b09022364b871d974ac6a4fa2a082f7d4aae8725
Blckguard <blckguard2000@gmail.com>
2023-02-07 20:46:38 +0900

    player can't move opponent's pieces, printing list of taken pieces for each player. new class attribute, value of pieces for score calculation

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
 import os
 
 class pieces:
-    def __init__(self, id, location):
+    def __init__(self, id, location, value):
         self.id = id
         self.location = location
+        self.value = value
         self.taken = False
 
-def create_objects():
+dict_white = {'Ra': 'a1', 'Nb': 'b1', 'Bc': 'c1', 'Qd': 'd1', 'Ke': 'e1', 'Bf': 'f1', 'Ng': 'g1', 'Rh': 'h1',
+                        'Pa': 'a2', 'Pb': 'b2', 'Pc': 'c2', 'Pd': 'd2', 'Pe': 'e2', 'Pf': 'f2', 'Pg': 'g2', 'Ph': 'h2'}
+dict_black = {'Ra': 'a8', 'Nb': 'b8', 'Bc': 'c8', 'Qd': 'd8', 'Ke': 'e8', 'Bf': 'f8', 'Ng': 'g8', 'Rh': 'h8',
+                        'Pa': 'a7', 'Pb': 'b7', 'Pc': 'c7', 'Pd': 'd7', 'Pe': 'e7', 'Pf': 'f7', 'Pg': 'g7', 'Ph': 'h7'}
+
+def create_objects(objects):
 
     # creating two lists of objects containing the id and location of pieces using the piece_location list
     # returning list of objects for black and white
 
-    pieces_white = {'Ra': 'a1', 'Nb': 'b1', 'Bc': 'c1', 'Qd': 'd1', 'Ke': 'e1', 'Bf': 'f1', 'Ng': 'g1', 'Rh': 'h1',
-                        'Pa': 'a2', 'Pb': 'b2', 'Pc': 'c2', 'Pd': 'd2', 'Pe': 'e2', 'Pf': 'f2', 'Pg': 'g2', 'Ph': 'h2'}
-    pieces_black = {'Ra': 'a8', 'Nb': 'b8', 'Bc': 'c8', 'Qd': 'd8', 'Ke': 'e8', 'Bf': 'f8', 'Ng': 'g8', 'Rh': 'h8',
-                        'Pa': 'a7', 'Pb': 'b7', 'Pc': 'c7', 'Pd': 'd7', 'Pe': 'e7', 'Pf': 'f7', 'Pg': 'g7', 'Ph': 'h7'}
+    values = {'R': 5, 'N': 3, 'B': 3, 'Q': 10, 'P': 1, 'K': 0}
 
-    objects_white = []
-    objects_black = []
+    object_list = []
 
-    for id, location in pieces_white.items():
-        a = pieces(id, location)
-        objects_white.append(a)
+    for id, location in objects.items():
+        value = values[id[0]]
+        a = pieces(id, location, value)
+        object_list.append(a)
 
-    for id, location in pieces_black.items():
-        a = pieces(id, location)
-        objects_black.append(a)
+    return object_list
 
-    return objects_white, objects_black
-
-objects_white, objects_black = create_objects()
+objects_white, objects_black = create_objects(dict_white), create_objects(dict_black)
 
 def create_board():
 
@@ -74,12 +73,77 @@ def create_board():
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
+def player_move(my_pieces, opponent_pieces, player):
+
+    # takes a move from the player in the format "Ra1a4" = Rook a1 to a4, takes the first letter of the piece to move, the field where it is on,
+    # and the field where it's supposed to go. 
+
+    move = input(player + ' to move: ')
+    piece_locations = [i.location for i in my_pieces]
+    columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
+    successful = False
+    while not successful:
+        for my_piece in my_pieces:
+
+            opponent = opponent_pieces[my_pieces.index(my_piece)]
+            # successful move
+            if all([move[0:2] == my_piece.id, move[1:3] == my_piece.location, move[3:5] not in piece_locations]):
+                my_piece.id = move[0:2]
+                my_piece.location = move[3:5]
+                successful = True
+            # checks if the move is proper format
+            elif len(move) != 5 or move[0].islower():
+                print('not proper format.\n')
+                move = input('Try again: ')
+            # checks if coordinates exist
+            elif any([int(move[2]) > 8, int(move[2]) < 1, int(move[4]) > 8, int(move[4]) < 1, move[1] not in columns, move[3] not in columns]):
+                print('Coordinates don\'t exist\n ')
+                move = input('Try again: ')
+            # checks if player is trying to move opponent piece
+            elif move[0:2] == opponent.id and move[1:3] == opponent.location:
+                print('That is not your piece.\n')
+                move = input('Try again: ')
+            # checks if piece exists on field
+            elif move[1:3] not in piece_locations:
+                print('None of your pieces on that field.\n')
+                move = input('Try again: ')
+            # checks if there is already a piece on the field to move to
+            elif move [3:5] in piece_locations:
+                print('There is already one of your pieces on that field.\n')
+                move = input('Try again: ')
+
+            # checks if piece was taken, if so changes 'location' to 0 and 'taken' to True
+            if my_piece.location == opponent.location:
+                opponent.taken = True
+                opponent.location = 0
+                print(opponent.id, opponent.location, opponent.taken)
+
+def taken_list(opponent_pieces):
+
+    # takes list of objects and returns list of taken pieces and score of these pieces
+
+    score = 0
+    taken_pieces = []
+
+    for piece in opponent_pieces:
+        if piece.taken:
+            score += piece.value
+            taken_pieces.append(piece.id[0])
+
+    return taken_pieces, score
+
 def draw_board():
 
-    # draws the board onto the console using the dictionary created in the create_board function
+    # draws the board onto the console using the dictionary created in the create_board function and draws a list of taken pieces + the players score
+
+    taken_white, score_white = taken_list(objects_black)
+    taken_black, score_black = taken_list(objects_white)
 
     board = create_board()
     return     f"""
+    Black: {score_black} {' '.join(taken_black)}
+    
     8 |{board["a8"]}|{board["b8"]}|{board["c8"]}|{board["d8"]}|{board["e8"]}|{board["f8"]}|{board["g8"]}|{board["h8"]}|
     7 |{board["a7"]}|{board["b7"]}|{board["c7"]}|{board["d7"]}|{board["e7"]}|{board["f7"]}|{board["g7"]}|{board["h7"]}|
     6 |{board["a6"]}|{board["b6"]}|{board["c6"]}|{board["d6"]}|{board["e6"]}|{board["f6"]}|{board["g6"]}|{board["h6"]}|
@@ -89,43 +153,9 @@ def draw_board():
     2 |{board["a2"]}|{board["b2"]}|{board["c2"]}|{board["d2"]}|{board["e2"]}|{board["f2"]}|{board["g2"]}|{board["h2"]}|
     1 |{board["a1"]}|{board["b1"]}|{board["c1"]}|{board["d1"]}|{board["e1"]}|{board["f1"]}|{board["g1"]}|{board["h1"]}|
        a b c d e f g h
+
+    White: {score_white} {' '.join(taken_white)}
     """
-
-def player_move(my_pieces, opponent_pieces, player):
-
-    # takes a move from the player in the format "Ra1a4" = Rook a1 to a4, takes the first letter of the piece to move, the field where it is on,
-    # and the field where it's supposed to go. 
-
-    move = input(player + ' to move: ')
-    piece_locations = [i.location for i in my_pieces]
-
-    successful = False
-    while not successful:
-        for my_piece in my_pieces:
-
-            opponent = opponent_pieces[my_pieces.index(my_piece)]
-
-            if all([move[0:2] == my_piece.id, move[1:3] == my_piece.location, move[3:5] not in piece_locations]):
-                my_piece.id = move[0:2]
-                my_piece.location = move[3:5]
-                successful = True
-
-            elif move [3:5] in piece_locations:
-                print('There is already one of your pieces on that field.\n')
-                move = input('Try again: ')
-
-            elif move[0] == opponent.id and move[1:3] == opponent.location:
-                print('That is not your piece.\n')
-                move = input('Try again: ')
-
-            elif len(move) != 5:
-                print('not proper format.\n')
-                move = input('Try again: ')
-
-            if my_piece.location == opponent.location:
-                opponent.taken = True
-                opponent.location = 0
-                print(opponent.id, opponent.location, opponent.taken)
 
 for i in range(9):
     cls()
